4de781ac831573885af78f93d20a0eae7e1cd502
Mohammed Haaroon <md.haaroon0710@gmail.com>
2025-01-26 15:00:26 +0530

    Added project setup files and updated requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+edot='-e .'
+def get_requirements(file_path:str)->List[str]: 
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if edot in requirements:
+            requirements.remove(edot)
+    return requirements
+
+setup(
+    name="mlproject",
+    version='0.0.1',
+    author='Haaroon',
+    author_email='reachmdhaaroon@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
